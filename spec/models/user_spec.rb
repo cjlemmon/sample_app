39ffc5a77248b9375c97d8d43bf45e2c8249ca07
Charles Lemmon <cjlemmon@yahoo.com>
2011-02-15 15:21:18 -0800

    work on users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe User do
   before :each do
-    @attr = { :name => 'Charles', :email => 'cjlemmon@yahoo.com'}
+    @attr = { :name => 'Charles',
+              :email => 'cjlemmon@yahoo.com',
+              :password => 'foobar',
+              :password_confirmation => 'foobar'
+            }
   end
 
   it 'should create a valid user given valid attributes' do
@@ -35,6 +39,31 @@ describe User do
     addresses.each do |address|
       invalid_email_user = User.new(@attr.merge(:email => address))
       invalid_email_user.should_not be_valid
+    end
+  end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
     end
   end
 
